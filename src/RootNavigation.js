0f7360e252e8f4ef000add64334e7e3489c04fd6
zxcv13579 <j0910290373@gmail.com>
2022-12-06 21:40:25 +0800

    修改 bottom tab navigation icon

--- src/RootNavigation.js
+++ src/RootNavigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import DiscussIcon from 'src/images/root/discuss.svg';
-import CustomIcon from 'src/images/root/custom.svg';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import DiscussNavigation from 'src/screens/DiscussNavigation';
 import CustomerNavigation from 'src/screens/CustomerNavigation';
@@ -11,15 +10,30 @@ const RootNavigation = () => {
   return (
     <Tab.Navigator
       initialRouteName="Discuss"
-      screenOptions={() => {
+      screenOptions={({route}) => {
         return {
           headerTitleAlign: 'center',
           headerShown: false,
           tabBarStyle: {backgroundColor: '#313131'},
-          tabBarLabelStyle: {fontSize: 16, color: 'white'},
-
+          tabBarLabelStyle: {fontSize: 14},
           tabBarActiveBackgroundColor: '#424242',
+          tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.4)',
+          tabBarActiveTintColor: 'rgba(255, 255, 255, 1)',
           unmountOnBlur: true,
+          tabBarIcon: ({focused}) => {
+            const icons = {
+              Discuss: 'briefcase',
+              Customer: 'people',
+            };
+
+            return (
+              <Ionicons
+                name={icons[route.name]}
+                color={focused ? 'white' : 'rgba(255, 255, 255, 0.4)'}
+                size={20}
+              />
+            );
+          },
         };
       }}>
       <Tab.Screen
@@ -27,18 +41,15 @@ const RootNavigation = () => {
         component={DiscussNavigation}
         options={{
           title: '商談',
-          tabBarIcon: () => <DiscussIcon />,
         }}
       />
       <Tab.Screen
-        name="Discusssd"
+        name="Customer"
         component={CustomerNavigation}
         options={{
           title: '顧客經營',
-          tabBarIcon: () => <CustomIcon />,
         }}
       />
-      {/* <Tab.Screen name="Discus3s" component={DiscussNavigation} /> */}
     </Tab.Navigator>
   );
 };
